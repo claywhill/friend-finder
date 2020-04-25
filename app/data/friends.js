9195d94332c038704122ce3ce84781f478a5cb0c
Clayton Hill <clayman0123@yahoo.com>
2020-04-25 16:27:13 -0600

    created friends and linked htmlroutes to files

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+        name: "Clayton",
+        photo: null,
+        scores: [5, 3, 4, 2, 1, 3, 3, 4, 4, 4]
+    },
+    {
+        name: "Marcus",
+        photo: null,
+        scores: [3, 5, 3, 2, 1, 4, 4, 1, 5, 5]
+    },
+    {
+        name: "Steve",
+        photo: null,
+        scores: [1, 5, 3, 4, 4, 4, 2, 5, 3, 2]
+    }
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require("express");
-var path = require("path");
-
 
 var app = express();
-var PORT = 3000;
+
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +15,6 @@ app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "survey.html"));
 })
 
-app.listen(PORT, function(req, res) {
+app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 })
